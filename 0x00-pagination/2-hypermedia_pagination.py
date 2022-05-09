392bc371b841197d5618959dde093c788ce7de4f
lepc1972 <vagrant@vagrant.vm>
2022-05-09 13:32:54 +0000

    hypermedia

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Pagination """
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """class to paginate a database
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cache dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return list of baby names or assert error
+        """
+        assert(isinstance(page, int) and isinstance(page_size, int))
+        assert(page > 0 and page_size > 0)
+        [start, end] = index_range(page, page_size)
+        return self.dataset()[start: end]
+
+    def get_hyper(self, page: int, page_size: int) -> dict:
+        """
+        Retuns a dictionary
+        """
+        dataset_records = self.get_page(page, page_size)
+
+        page_dict = math.ceil(len(self.__dataset) / page_size)
+
+        return {
+            'page_size': len(dataset_records),
+            'page': page,
+            'data': dataset_records,
+            'next_page': page + 1 if (page + 1) <= page_dict else None,
+            'prev_page': page - 1 if (page - 1) > 0 else None,
+            'total_pages': page_dict
+        }
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index and an end
+    index
+    """
+    return ((page - 1) * page_size, page * page_size)
